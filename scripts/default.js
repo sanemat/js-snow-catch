0a39cc1acd9511c3623e83742f5b992cbe4ff5ac
Matt (Sanemat) <o.gata.ken@gmail.com>
2019-08-27 22:35:44 +0700

    snows

--- scripts/default.js
+++ scripts/default.js
@@ -46,11 +46,20 @@
   //Sprite を扱う変数オブジェクト
   let sprite = {
     //雪の結晶 Sprite が格納
-    snow: null,
+    snows: [],
     //雪だるまの Sprite が格納
     snow_man: null
   };
-  /*ここに演習 6 タスク 1 で SNOWS_MOVING_CON オブジェクト変数を定義します。*/
+  //雪の結晶の動作が使用する定数
+  let SNOWS_MOVING_CONF = {
+    //表示する雪の結晶の数
+    count: 7,
+    //隣り合う 雪の結晶画像の x 位置の差分
+    neighor_distance: 56,
+    /* ここに演習 6 タスク 2 で start_coefficient プロパティを記述します。*/
+    /* ここに演習 7 タスク 1 で switch_count プロパティを記述します。*/
+  };
+  /* ここに演習 7 タスク 1 でスプライト関連の変数をいくつか記述します。*/
   /*ここに演習 8 で Rule クラスを定義します*/
 
   //ゲームに必要なアセットをロードする
@@ -74,17 +83,19 @@
     /*画像読み込み完了の Canvas に
     画像を表示するメソッドを記述 */
     img.snow.onload = () => {
-      /*このハンドラの処理は演習 6 のタスク 1 手順 3 で書き換えます*/
-      /*ここは演習 7 のタスク 1 手順 3 で変更されます*/
-      //画像を引数に Sprite クラスのインスタンスを生成
-      sprite.snow = new Sprite(img.snow);
-
-      //以下の行が書き換えた箇所です
-      sprite.snow.x = getCenterPosition(canvas.clientWidth, sprite.snow.width);
-
-      sprite.snow.y = 0;
-      //Sprite オブジェクトに定義した draw メソッドで描画
-      sprite.snow.draw();
+      for (let i = 0; i < SNOWS_MOVING_CONF.count; i++) {
+        //画像を引数に Sprite クラスのインスタンスを生成
+        /*ここは演習 7 のタスク 1 手順 3 で変更されます*/
+        let sprite_snow = new Sprite(img.snow);
+        sprite_snow.dy = 1;
+        sprite_snow.dx = SNOWS_MOVING_CONF.neighor_distance;
+        sprite_snow.x = i * sprite_snow.dx;
+        /*演習 6 のタスク 2 ステップ 3 で
+        sprite_snow.y を getRandomPosition 関数でセット*/
+        /*ここに演習 7 のタスク 2 でオーディオ再生用のコードを追加します*/
+        sprite.snows.push(sprite_snow);
+        sprite_snow = null;
+      }
     };
     //雪だるまインスタンスの生成
     img.snow_man = new Image();
@@ -135,27 +146,35 @@
   }
 
   function renderFrame() {
-    //sprite.snow の y 値(縦位置) が canvas からはみ出たら先頭に戻す
-    if (sprite.snow.y > canvas.clientHeight) {
-      sprite.snow.y = 0
-    }
     //canvas をクリア
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    //sprite.snow の y 値を増分
-    sprite.snow.y += 2;
     // sprite.snow_man の x 値が動作範囲内かどうか
     if ((sprite.snow_man.x < sprite.snow_man.limit_rightPosition && key_value > 0)
       || (sprite.snow_man.x >= 3 && key_value < 0)) {
       //sprite.snow_man の x 値を増分
       sprite.snow_man.x += key_value;
     }
-    //Spriteを描画
-    sprite.snow.draw();
-    sprite.snow_man.draw();
-    ///あたり判定
-    if (isHit(sprite.snow, sprite.snow_man)) {
-      hitJob()
+    let length = sprite.snows.length;
+    for (let i = 0; i < length; i++) {
+      let sprite_snow = sprite.snows[i];
+      //sprite_snow の y 値(縦位置) が canvas からはみ出たら先頭に戻す
+      if (sprite_snow.y > canvas.clientHeight) {
+        /*ここに演習 8 で gameRule.isCatched メソッドを追加します*/
+        sprite_snow.y = 0;
+        /* ここに演習 7 のタスク 1 手順 8 でコードを追加します*/
+      }
+      //sprite_snow の y 値を増分
+      sprite_snow.y += sprite_snow.dy;
+      //Spriteを描画
+      sprite_snow.draw();
+      //当たり判定
+      if (isHit(sprite_snow, sprite.snow_man)) {
+        hitJob()
+      }
+      sprite_snow = null;
     }
+    //Spriteを描画
+    sprite.snow_man.draw();
 
     /*ここに演習 7 のタスク 1 手順 7 で処理数のカウントを追加します*/
     //ループを開始
